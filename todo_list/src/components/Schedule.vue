a5e10b9e8a77989a1cb90fea5ecdce4be2108658
Bruno Murozaki <you@example.com>
2017-07-04 01:15:51 -0300

    Primeiros testes com o vue

---
+++ todo_list/src/components/Schedule.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="schedule">
+    <header-component></header-component>
+  </div>
+</template>
+
+<script>
+
+import HeaderComponent from './ScheduleHeader.vue'
+
+export default {
+  name: 'schedule',
+  components: {
+    HeaderComponent
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
---
+++ todo_list/src/components/ScheduleHeader.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="header">
+    <div class="headerTitle"><h1>Agenda</h1></div>
+  	<div class="menuWrapper">
+	  <div class="taskOptions">
+	    <button class="btn">Add Task</button>
+	  </div>
+   	  <div class="scheduleOptions">       
+        <button class="btn">Dia</button>
+        <button class="btn">Semana</button>
+        <button class="btn">Mes</button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'header'
+}
+</script>
+
+<style>
+
+.header{width:100%; height:20%; border-bottom: #000 solid 1px; position:fixed}
+.headerTitle>h1{margin-top:0px}
+.menuWrapper{float:left; width:100%; padding:0 10px}
+.scheduleOptions{float:right;}
+.taskOptions{float:left}
+
+</style>
